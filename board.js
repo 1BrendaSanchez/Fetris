85b531d8a15c6d21f0224cb822b51284b47fa735
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-03-15 14:05:24 -0700

    finished tetrominoes

--- board.js
+++ board.js
@@ -1,10 +1,13 @@
+/*
 class Board {
   // ctx (context) is the object that provides methods and properties for drawing on the canvas.
   constructor(ctx) {
     this.ctx = ctx;
     this.grid = this.getEmptyBoard();
+    this.piece = new Piece(ctx);
   }
   getEmptyBoard() {
     return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
   }
 }
+  */
--- main.js
+++ main.js
@@ -4,16 +4,8 @@ const BLOCK_SIZE = 30;
 
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
-// let canvas = document.getElementById("canvas");
-// let ctx = canvas.getContext("2d");
-ctx.fillStyle = "red";
-// ctx.fillRect(x, y, width, height);
 
-// const { width, height } = this.ctx.canvas;
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, 10, 10);
-// ctx.clearRect(0, 0, width, height);
-ctx.fillRect(1, 1, 10, 10);
+let board = null;
 
 // Calculate size of canvas from constants.
 ctx.canvas.width = COLS * BLOCK_SIZE;
@@ -22,9 +14,55 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 // Scale blocks
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-let board = new Board(ctx);
+class Board {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.grid = this.getEmptyBoard();
+    this.piece = new Piece(ctx);
+  }
+
+  // Get matrix filled with zeros.
+  getEmptyBoard() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
+}
+
+class Piece {
+  constructor(ctx) {
+    this.ctx = ctx;
+
+    this.color = "blue";
+    this.shape = [
+      [2, 0, 0],
+      [2, 2, 2],
+      [0, 0, 0],
+    ];
+
+    // Starting position.
+    this.x = 3;
+    this.y = 0;
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
+    });
+  }
+}
 
 function play() {
   board = new Board(ctx);
-  console.table(board.grid);
+  draw();
+}
+
+function draw() {
+  const { width, height } = ctx.canvas;
+  ctx.clearRect(0, 0, width, height);
+
+  board.piece.draw();
 }
