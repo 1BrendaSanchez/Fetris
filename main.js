b3965cae6c2233b1de1c2e28df268f5b7445e8ee
BrendalisSanchez <brendalis.sanchez2018@gmail.com>
2025-03-15 13:47:10 -0700

    finished canvas lesson

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
+// let canvas = document.getElementById("canvas");
+// let ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+// ctx.fillRect(x, y, width, height);
+
+// const { width, height } = this.ctx.canvas;
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 10, 10);
+// ctx.clearRect(0, 0, width, height);
+ctx.fillRect(1, 1, 10, 10);
 
 // Calculate size of canvas from constants.
 ctx.canvas.width = COLS * BLOCK_SIZE;
